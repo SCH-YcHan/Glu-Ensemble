cd4a442f170453fc3ecea0cc9d1d36b8de739436
SCH-YcHan <ychan.sch@gmail.com>
2023-03-22 22:37:11 +0900

    Modify

---
+++ code/BG EDA.R
@@ -0,0 +1,69 @@
+rm(list=ls())
+
+#install.packages(c("openxlsx", "dplyr", "stringr", "lubridate"))
+
+library(openxlsx)
+library(dplyr)
+library(stringr)
+library(lubridate)
+
+xlsx_folder_path <- "../data/BG data/"
+xlsx_file_names <- list.files(xlsx_folder_path)
+
+if(!file.exists("../data/BG EDA")){
+  dir.create("../data/BG EDA")
+}
+
+#columns rename and cutting row
+for(file_name in xlsx_file_names){
+  f <- read.xlsx(paste0(xlsx_folder_path, file_name))
+  names(f) <- c("index", "time_stamp", "event", "event_sub",
+                "patient", "device", "source",
+                "glucose", "insulin", "carbo",
+                "time", "glu_v", "tm", "tm_id")
+
+  f2 <- f %>%
+    select(time_stamp, event, glucose, insulin, carbo) %>%
+    filter(!is.na(time_stamp))
+
+  write.csv(f2, paste0("../data/BG EDA/", strsplit(file_name, "\\.")[[1]][1], ".csv"), row.names = F)
+}- csv_folder/data/BG EDA/"
+csv_names <-csv_filest.files("../data/BG csv_folder_path? ??Checking files for strange blood glucose valuese <- data.frame()
+for(csv_name in csv_names){
+  f file_<- read.csv(paste0(csv_path, csv_nafolder_me))
+  
+  if(!is.numeric(f$glucose)){
+    row <- table(f$glucose) %>% 
+      data.frame %>% 
+      mutate(Var1=ifelse(is.na(as.numeric(as.character(Var1))), as.character(Var1), NA)) %>% 
+      filter(!is.na(Var1)) %>% 
+      cbind(File=strsplit(csv_name, "\\.")[[1]][1],.) %>% 
+      mutate(ratio = round(Freq/nrow(f), 3))
+    
+    not_bg_value <- rbind(not_bg_value, row)
+  }
+}
+not_bg_value #?????? ????){(x, na.rm=T)
+  max_v <- max(x, na.rm=T)
+  
+  return(paste0(min_v, " ~ ", max_v))
+}
+
+#?ν??? or ź??ȭ?? ?? ??�� Ȯ??
+ran#Check the range of blood glucose measurement intervals
+me in csv_names){
+  f <- read.csv(paste0(csv_path,file_ csv_name))
+  f$time_stamp <- ymd_hfolder_ms(f$time_stamp)
+  
+  row <- f %>% 
+    filter(event=="EGV") %>% 
+    mutate(time_lag = as.duration(interval(lag(time_stamp), time_stamp))) %>% 
+    mutate(time_sec = time_lag/as.duration(seconds(1))) %>% 
+    summarise_at(.vars=c("time_sec"), .funs=c("range")) %>% 
+    cbind(File=strsplit(csv_name, "\\.")[[1]][1], .)
+  
+  time_interval <- rbind(time_interval, row)
+}
+time_interval
+
+f <- read.csv(paste0(csv_path, "S002.csv
